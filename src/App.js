3edb836093b44fb31eea09ef581e209f05b48545
AxSch <alex_schuneman@outlook.com>
2020-02-18 12:48:37 +0000

    Implement TabContainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import welcomeIcon from './assets/welcome.jpg';
+import TabContainer from './Containers/TabContainer';
 
 class App extends Component {
   render() {
     return (
-      <img src={welcomeIcon} alt="Welcome!"/>
+      <>
+        <img src={welcomeIcon} alt="Welcome!" />
+        <TabContainer />
+      </>
     );
   }
 }
