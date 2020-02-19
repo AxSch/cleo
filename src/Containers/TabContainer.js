8582a03c6d1fe181a9d212d28e371a6e36f6860a
AxSch <alex_schuneman@outlook.com>
2020-02-19 00:14:46 +0000

    Add addBill() , removeBill() & sortData()

--- src/Containers/TabContainer.js
+++ src/Containers/TabContainer.js
@@ -7,27 +7,60 @@ class TabContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            billData: []
+            billData: [],
+            sortedBillData: null
         }
+        this.removeBill = this.removeBill.bind(this)
+        this.addBill = this.addBill.bind(this)
+    }
+
+    sortData(billData) {
+        const sortedData = {
+            bills: [],
+            potentialBills: []
+        }
+    
+        billData.forEach(bill => {
+            if (bill.isBill) {
+                sortedData.bills.push(bill)
+            } else {
+                sortedData.potentialBills.push(bill)
+            }
+        })
+        this.setState({sortedBillData: sortedData})
     }
 
     async fetchData() {
         const req = await axios.get(APIConstants.base + 'bills/')
         this.setState({billData: req.data})
+    }
 
+    async removeBill(id) {
+        await axios.patch(APIConstants.base + 'bills/' + id, { isBill: false})
+        this.fetchData()
+    }
+
+    async addBill(id) {
+        await axios.patch(APIConstants.base + 'bills/' + id, { isBill: true})
+        this.fetchData()
     }
     
     componentDidMount() {
         this.fetchData()
     }
 
-    render() {
+    componentDidUpdate(prevProps, prevState) {
         const { billData } = this.state
-        console.log(billData)
+        if (billData !== prevState.billData) {
+            this.sortData(billData)
+        }
+    }
+    
+    render() {
+        const { sortedBillData } = this.state
         return (
             <>
-                HI
-                <TabDetail billData={billData} />
+                <TabDetail billData={sortedBillData} removeBill={this.removeBill}  addBill={this.addBill}/>
             </>
         )
     }
